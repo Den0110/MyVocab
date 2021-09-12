3e0a53f478907130efc62fed3cbb73528338a152
Den0110 <denkov0110@yandex.ru>
2021-09-13 00:26:58 +0300

    refactor(dependencies): dependencies updated

--- app/src/main/java/com/myvocab/myvocab/util/BaseViewModelFactory.kt
+++ app/src/main/java/com/myvocab/myvocab/util/BaseViewModelFactory.kt
@@ -8,7 +8,7 @@ import androidx.lifecycle.ViewModelProvider
 @Suppress("UNCHECKED_CAST")
 class BaseViewModelFactory<T>(val creator: () -> T) : ViewModelProvider.Factory {
 
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return creator() as T
     }
 
@@ -16,14 +16,14 @@ class BaseViewModelFactory<T>(val creator: () -> T) : ViewModelProvider.Factory 
 
 inline fun <reified T : ViewModel> Fragment.getViewModel(noinline creator: (() -> T)? = null): T {
     return if (creator == null)
-        ViewModelProvider(this).get(T::class.java)
+        ViewModelProvider(this)[T::class.java]
     else
-        ViewModelProvider(this, BaseViewModelFactory(creator)).get(T::class.java)
+        ViewModelProvider(this, BaseViewModelFactory(creator))[T::class.java]
 }
 
 inline fun <reified T : ViewModel> FragmentActivity.getViewModel(noinline creator: (() -> T)? = null): T {
     return if (creator == null)
-        ViewModelProvider(this).get(T::class.java)
+        ViewModelProvider(this)[T::class.java]
     else
-        ViewModelProvider(this, BaseViewModelFactory(creator)).get(T::class.java)
+        ViewModelProvider(this, BaseViewModelFactory(creator))[T::class.java]
 }
